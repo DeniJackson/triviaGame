0ce512a114a3c4965fc1b6517256bfaff0a7d204
DJ <konobeat@gmail.com>
2018-06-06 19:07:22 -0400

    The trivia game now works. Needs to be refactored.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,4 @@
-var pokemon = [];
-var counter = 4;
-var activePokemon;
-var counterTimer;
-var timeoutTimer;
+
 var vulpix = {
     name:'vulpix',
     image:'vulpix',
@@ -17,44 +13,72 @@ var shaymin = {
     correct:'Shaymin',
     wrong2:'Eevee',
 }
+var squirtle = {
+    name:'Squirtle',
+    image: 'squirtle',
+    wrong1:'Warturtle',
+    correct:'Squirtle',
+    wrong2:'Snivy',
+}
 
-
+var butterfree = {
+    name:'butterfree',
+    wrong1:'Venomoth',
+    wrong2:'Beedrill',
+    correct:'Butterfree'
+}
+var venonat = {
+    name:'Venonat',
+    wrong1:'Vileplume',
+    wrong2:'Tangla',
+    correct:'Venonat'
+}
 
 
 $('.getStarted').on('click', function(){
+    
+    counter = 4;
+    correct = 0;
+    incorrect = 0;
     counterStart();
+    $('.pokemonImage').removeClass('venonat2');
+    console.log('button clicked')
     
-    timeOut();
     activePokemon = vulpix;
-    
-
-    $(this).toggleClass('block');
-    $(this).toggleClass('hide');
-    $('.pokemonImage').toggleClass('hide');
-    $('.question').toggleClass('hide');
-    $('.pokemonImage').addClass('vulpix')
+    $('.stats').addClass('hide');
+    timeOut();
+    $(this).removeClass('block');
+    $(this).addClass('hide');
+    $('.pokemonImage').removeClass('hide');
+    $('.question').removeClass('hide');
+    $('.pokemonImage').addClass('vulpix');
     $('.pokemonImage').css('background-size', 'cover');
-    $('li:first').append(vulpix.correct);
-    $('li:first').addClass('correct')
-    $('li:nth-of-type(2)').append(vulpix.wrong1);
-    $('li:last').append(vulpix.wrong2);
-    $('ol').toggleClass('hide');
+    $('li:first').text(vulpix.correct);
+    $('li:first').addClass('correct');
+    $('li:nth-of-type(2)').text(vulpix.wrong1);
+    $('li:last').text(vulpix.wrong2);
+    $('ol').removeClass('hide');
+    $('li').off();
 
     $('li').on('click', function(){
-        console.log(this + 'clicked');
+  
         if ($(this).hasClass('correct')){
             clearTimeout(timeoutTimer);
             $('.pokemonImage').removeClass('vulpix');
             $('.pokemonImage').addClass('vulpix2');
             $('.question').text("Correct!");
-            $('ol').toggleClass('hide');
+            $('ol').addClass('hide');
             clearInterval(counterTimer);
-            nextPokemon();
+            
+            correct++;
 
         }else{
             wrong();
-            nextPokemon();
+            incorrect++;
+            
         }
+        
+        nextPokemon();
     })
     
     
@@ -67,15 +91,33 @@ function counterStart(){
 
 function timeOut() {
     timeoutTimer = setTimeout (function(){
-    $('.pokemonImage').removeClass('vulpix');
-    $('.pokemonImage').addClass('vulpix2');
-    $('.question').text("Time's up!");
+    
+    $('.question').text("Time's up! The answer was " + activePokemon.name + '.');
     $('ol').toggleClass('hide');
-    clearInterval(counterTimer);
+    incorrect++;
+    counter  = 4;
     if(activePokemon == vulpix){
+        $('.pokemonImage').removeClass('vulpix');
+        $('.pokemonImage').addClass('vulpix2');
         nextPokemon();
+    } else if (activePokemon == shaymin){
+        $('.pokemonImage').removeClass('shaymin');
+        $('.pokemonImage').addClass('shaymin2');
+        thirdPokemon();
+    } else if(activePokemon == squirtle) {
+        $('.pokemonImage').removeClass('squirtle');
+        $('.pokemonImage').addClass('squirtle2');
+        fourthPokemon();
+    } else if (activePokemon == butterfree){
+        $('.pokemonImage').removeClass('butterfree');
+        $('.pokemonImage').addClass('butterfree2');
+        fifthPokemon();
+    } else if (activePokemon == venonat){
+        $('.pokemonImage').removeClass('venonat');
+        $('.pokemonImage').addClass('venonat2');
+        showstats();
     }
-
+    clearInterval(counterTimer);
     }, 5000)
 }
 function wrong(){
@@ -85,6 +127,15 @@ function wrong(){
     } else if (activePokemon == shaymin){
         $('.pokemonImage').removeClass('shaymin');
         $('.pokemonImage').addClass('shaymin2');
+    } else if (activePokemon == squirtle){
+        $('.pokemonImage').removeClass('squirtle');
+        $('.pokemonImage').addClass('squirtle2');
+    } else if (activePokemon == butterfree){
+        $('.pokemonImage').removeClass('butterfree');
+        $('.pokemonImage').addClass('butterfree2');
+    } else{
+        $('.pokemonImage').removeClass('venonat');
+        $('.pokemonImage').addClass('venonat2');
     }
          
         $('.question').text("Wrong answer! It was " + activePokemon.name +'.');
@@ -99,19 +150,21 @@ function nextPokemon(){
         counter = 4;
         counterStart();
         timeOut();
-        $('.question').text("Who's That Pokemon?")
-        $('.pokemonImage').addClass('shaymin')
+        $('.question').text("Who's That Pokemon?");
+        $('.pokemonImage').removeClass('vulpix2');
+        $('.pokemonImage').addClass('shaymin');
         $('.pokemonImage').css('background-size', 'cover');
-        $('li:first').removeClass('correct')
+        $('li:first').removeClass('correct');
         $('li:first').text(shaymin.wrong2);
         $('li:nth-of-type(2)').text(shaymin.wrong1);
         $('li:last').text(shaymin.correct);
-        $('li:last').addClass('correct')
+        $('li:last').addClass('correct');
         $('ol').toggleClass('hide');
         $('li').off();
+        
 
         $('li').on('click', function(){
-            console.log(this + 'clicked');
+         
             if ($(this).hasClass('correct')){
                 clearTimeout(timeoutTimer);
                 $('.pokemonImage').removeClass('shaymin');
@@ -119,14 +172,149 @@ function nextPokemon(){
                 $('.question').text("Correct!");
                 $('ol').toggleClass('hide');
                 clearInterval(counterTimer);
+                correct++;
     
             }else{
                 wrong();
+                incorrect++;
             }
+
+            
+            thirdPokemon();
+    })
     
+},5000)
+}
+
+function thirdPokemon(){
+    activePokemon = squirtle;
+    setTimeout (function(){
+        counter = 4;
+        counterStart();
+        timeOut();
+        $('.question').text("Who's That Pokemon?");
+        $('.pokemonImage').removeClass('shaymin2');
+        $('.pokemonImage').addClass('squirtle');
+        $('.pokemonImage').css('background-size', 'cover');
+        $('li:first').addClass('correct');
+        $('li:last').removeClass('correct');
+        $('li:first').text(squirtle.correct);
+        $('li:nth-of-type(2)').text(squirtle.wrong2);
+        $('li:last').text(squirtle.wrong1);
+        $('ol').toggleClass('hide');
+        $('li').off();
+
+        $('li').on('click', function(){
+           
+            if ($(this).hasClass('correct')){
+                clearTimeout(timeoutTimer);
+                $('.pokemonImage').removeClass('squirtle');
+                $('.pokemonImage').addClass('squirtle2');
+                $('.question').text("Correct!");
+                $('ol').toggleClass('hide');
+                clearInterval(counterTimer);
+                correct++;
+    
+            }else{
+                wrong();
+                incorrect++;
+            }
+            fourthPokemon();
+    
+    })
+        
+
+},5000)
+}
+function fourthPokemon(){
+    activePokemon = butterfree;
+    setTimeout (function(){
+        counter = 4;
+        counterStart();
+        timeOut();
+        $('.question').text("Who's That Pokemon?");
+        $('.pokemonImage').removeClass('squirtle2');
+        $('.pokemonImage').addClass('butterfree');
+        $('.pokemonImage').css('background-size', 'cover');
+        $('li:first').removeClass('correct');
+        $('li:nth-of-type(2)').addClass('correct');
+        $('li:first').text(butterfree.wrong2);
+        $('li:nth-of-type(2)').text(butterfree.correct);
+        $('li:last').text(butterfree.wrong1);
+        $('ol').toggleClass('hide');
+        $('li').off();
+
+        $('li').on('click', function(){
+        
+            if ($(this).hasClass('correct')){
+                clearTimeout(timeoutTimer);
+                $('.pokemonImage').removeClass('butterfree');
+                $('.pokemonImage').addClass('butterfree2');
+                $('.question').text("Correct!");
+                $('ol').toggleClass('hide');
+                clearInterval(counterTimer);
+                correct++;
+    
+            }else{
+                wrong();
+                incorrect++;
+            }
+            fifthPokemon();
+    })
+
+},5000)
+}
+function fifthPokemon(){
+    activePokemon = venonat;
+    setTimeout (function(){
+        counter = 4;
+        counterStart();
+        timeOut();
+        $('.question').text("Who's That Pokemon?");
+        $('.pokemonImage').removeClass('butterfree2');
+        $('.pokemonImage').addClass('venonat');
+        $('.pokemonImage').css('background-size', 'cover');
+        $('li:last').addClass('correct');
+        $('li:nth-of-type(2)').removeClass('correct');
+        $('li:first').text(venonat.wrong1);
+        $('li:nth-of-type(2)').text(venonat.wrong2);
+        $('li:last').text(venonat.correct);
+        $('ol').toggleClass('hide');
+        $('li').off();
+
+        $('li').on('click', function(){
+           
+            if ($(this).hasClass('correct')){
+                clearTimeout(timeoutTimer);
+                $('.pokemonImage').removeClass('venonat');
+                $('.pokemonImage').addClass('venonat2');
+                $('.question').text("Correct!");
+                $('ol').toggleClass('hide');
+                clearInterval(counterTimer);
+                
+    
+            }else{
+                wrong();
+                
+            }
+            
+            showstats();
     })
 
 },5000)
 }
 
-
+function showstats(){
+    $('.stats').removeClass('hide');
+    $('.correctAnswers').text('Correct: ' + correct);
+    $('.incorrectAnswers').text('Incorrect: ' + incorrect);
+    $('button').addClass('block');
+    $('button').removeClass('hide').text('Restart?');
+    pokemon = [];
+    counter = 4;
+    activePokemon;
+    counterTimer;
+    timeoutTimer;
+    correct = 0;
+    incorrect = 0;
+}
